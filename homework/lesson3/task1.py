842dd09ffa73621dc056ccda141fd315d9025914
D1zarm <featweath1337@gmail.com>
2023-04-27 14:12:58 +0300

    lesson 3 homework

---
+++ homework/lesson3/task1.py
@@ -0,0 +1,15 @@
+# Задача 1. Создайте список. Запишите в него N первых элементов последовательности Фибоначчи.
+# 6 –> 1 1 2 3 5 8
+
+def fib(n):
+    lst = []
+    if n == 0:
+        return [0]
+    elif n == 1:
+        return [0, 1]
+    else:
+        lst = fib(n-1)
+        lst.append(lst[-1] + lst[-2])
+        return lst
+n = input('Введите челое число N: ')
+print(fib(int(n)))
